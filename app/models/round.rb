428f9d28251c65dc8ba9f9d4d27a46c43ae247d8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 17:05:02 -0500

    added user profile, create, and update views

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,9 @@ class Round < ActiveRecord::Base
   belongs_to :deck
   delegate :cards, to: :deck
   has_many :guesses, dependent: :destroy
+
+  def percentage_correct
+    ((self.guesses.where('correct = ?', true).count / self.cards.size.to_f) * 100).round
+  end
+  
 end
