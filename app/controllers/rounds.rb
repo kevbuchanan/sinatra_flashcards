61fd715a3b3f2a6529373abe83e14bff7a966bc2
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 14:56:36 -0500

    added more round routes

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,4 +1,5 @@
-post 'round/new/deck/:deck_id' do
+# Round#create
+get '/round/new/deck/:deck_id' do
   # always a current user. (id = 1 is guest_user)
   round = Round.new(deck: params[:deck_id], user: current_user.id )
 
