aae8b65d5b4b8259e5fe6be43edb0963bebef013
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 12:40:49 -0500

    back from lunch adding kevin's changes to the index routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,23 @@
 get '/' do
   # Look in app/views/index.erb
+  # by default, need guest user (user.id = 1)
   erb :index
+end
+
+post '/login' do
+  "Set current_user if user/password valid \
+  rerender index for current_user"
+  redirect to :/
+end
+
+get '/signup' do
+  "Sign up Page"
+end
+
+get '/deck/:id' do
+  # get flashcards by id
+  # @deck = Deck.find(params[:id])
+
+  @deck = Deck.find(params[:id])
+  erb :"deck/show"
 end
