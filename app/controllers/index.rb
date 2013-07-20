90e1d861ec1243c1d843cf6c043fc805057285ce
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 20:12:49 -0500

    added proper login function

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,19 +1,7 @@
 get '/' do
   # Look in app/views/index.erb
-  # by default, need guest user (user.id = 1)
-  session[:user_id] = 1 unless logged_in?
-  
+  session[:user_id] = 1 unless logged_in? # by default, need guest user (user.id = 1)
   erb :index
-end
-
-post '/login' do
-  "Set current_user if user/password valid \
-  rerender index for current_user"
-  redirect to :/
-end
-
-get '/signup' do
-  "Sign up Page"
 end
 
 get '/deck/:id' do
