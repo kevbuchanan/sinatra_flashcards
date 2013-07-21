caa8e4a13d74c6dcd377a331697e13fe0527fb1c
William Karsten <yme_ynot45@hotmail.com>
2013-07-21 16:53:49 -0500

    fixed the seed file so that all 6 decks have cards.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,34 @@
 require 'faker'
 
 my_deck = []
+my_deck2 = []
+my_deck3 = []
+my_deck4 = []
+my_deck5 = []
+my_deck6 = []
 
 10.times do |i|
   my_deck << Card.create(question: "Why Not Zoidberg?", answer: "cause")
+end
+
+10.times do |i|
+  my_deck2 << Card.create(question: "Why Not Zoidberg?", answer: "cause")
+end
+
+10.times do |i|
+  my_deck3 << Card.create(question: "Why Not Zoidberg?", answer: "cause")
+end
+
+10.times do |i|
+  my_deck4 << Card.create(question: "Why Not Zoidberg?", answer: "cause")
+end
+
+10.times do |i|
+  my_deck5 << Card.create(question: "Why Not Zoidberg?", answer: "cause")
+end
+
+10.times do |i|
+  my_deck6 << Card.create(question: "Why Not Zoidberg?", answer: "cause")
 end
 
 Deck.create(  title: "Russian_Quiz",
@@ -14,32 +39,31 @@ Deck.create(  title: "Russian_Quiz",
 
 Deck.create(  title: "French Quiz",
               user_id: 1,
-              cards: my_deck,
+              cards: my_deck2,
               description: "French/English Basic Vocabulary (food, places, cigarettes, etc)"
             )
 
 Deck.create(  title: "German Quiz",
               user_id: 1,
-              cards: my_deck,
+              cards: my_deck3,
               description: "German/English Basic Vocabulary (philosophy, fascism, revisionist history, etc)"
             )
 
-
 Deck.create(  title: "Ruby Quiz",
               user_id: 1,
-              cards: my_deck,
+              cards: my_deck4,
               description: "Ruby/English Basic Vocabulary (clean syntax)"
             )
 
 Deck.create(  title: "C+ Quiz",
               user_id: 1,
-              cards: my_deck,
+              cards: my_deck5,
               description: "C+/English Basic Vocabulary (video games)"
             )
 
 Deck.create(  title: "Clojure Quiz",
               user_id: 1,
-              cards: my_deck,
+              cards: my_deck6,
               description: "Clojure/English Basic Vocabulary (security loopholes)"
             )
 
